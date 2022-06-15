9d6e4a1fe2c78c6c4ddad03b860f6cb27f695a02
Furkan Kadıoğlu <iam@frkn.com.tr>
2022-06-16 02:25:11 +0300

    Cities resource and collection added

---
+++ app/Http/Resources/CityCollection.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class CityCollection extends ResourceCollection
+{
+
+    public $collects = CityResource::class;
+
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return parent::toArray($request);
+    }
+}
