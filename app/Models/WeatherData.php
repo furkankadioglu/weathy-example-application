130beeb9538509e8ffc51f534ad8f3d423e6fd06
Furkan Kadıoğlu <iam@frkn.com.tr>
2022-06-15 03:47:45 +0300

    Fillable fields and city relation added

--- app/Models/WeatherData.php
+++ app/Models/WeatherData.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class WeatherData extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['city_id', 'date', 'data'];
+
+    public function city()
+    {
+        return $this->hasOne(City::class);
+    }
 }
